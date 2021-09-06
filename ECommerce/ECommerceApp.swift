c45b80c0408196adcce05552c9723273cd83a719
katie-mcbratney <88895553+katie-mcbratney@users.noreply.github.com>
2021-09-06 11:05:20 -0700

    Initial Commit

---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by Katie McBratney on 9/6/21.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
